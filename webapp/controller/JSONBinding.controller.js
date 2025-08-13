ba239a028c34fa2c4bf14408b5509e64993d5aaa
Jason Patigayon <jason.patigayon@accenture.com>
2025-08-13 15:57:15 +0000

    3rd commit

--- webapp/controller/JSONBinding.controller.js
+++ webapp/controller/JSONBinding.controller.js
@@ -9,57 +9,16 @@ sap.ui.define([
         formatter: formatter,
 
         onInit: function () {
-            
-            var oProductModel = new JSONModel({
-                Product: [
-                    {
-                        ProductID: 1,
-                        ProductName: "Product 1",
-                        SupplierID: 1,
-                        CategoryID: 1,
-                        QuantityPerUnit: "10 boxes x 20 bags",
-                        UnitPrice: 18.0,
-                        UnitsInStock: 39,
-                        UnitsOnOrder: 0,
-                        ReorderLevel: 10,
-                        Discontinued: false
-                    },
-                    {
-                        ProductID: 2,
-                        ProductName: "Product 2",
-                        SupplierID: 1,
-                        CategoryID: 1,
-                        QuantityPerUnit: "24 - 12 oz bottles",
-                        UnitPrice: 19.0,
-                        UnitsInStock: 17,
-                        UnitsOnOrder: 40,
-                        ReorderLevel: 25,
-                        Discontinued: false
-                    },
-                    {
-                        ProductID: 3,
-                        ProductName: "Product 3",
-                        SupplierID: 1,
-                        CategoryID: 2,
-                        QuantityPerUnit: "12 - 550 ml bottles",
-                        UnitPrice: 10.0,
-                        UnitsInStock: 13,
-                        UnitsOnOrder: 70,
-                        ReorderLevel: 25,
-                        Discontinued: false
-                    }
-                ],
-                CurrencyCode: "USD"
-            });
+            var oProductModel = new JSONModel("model/ProductModel.json");
             this.getView().setModel(oProductModel);
 
             var oInfoModel = new JSONModel({
                 EID: "",
                 Enabled: false,
                 Address: {
-                    Street: "123 Main St",
-                    City: "Cebu City",
-                    Zip: "6000",
+                    Street: "River Street",
+                    City: "Mandaue City",
+                    Zip: "6014",
                     Country: "Philippines"
                 },
                 SalesAmount: 1000,
@@ -67,6 +26,7 @@ sap.ui.define([
             });
             this.getView().setModel(oInfoModel, "info");
         },
+
 
         onItemPress: function (oEvent) {
             var oSelectedItem = oEvent.getSource();
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,16 +1,30 @@
-sap.ui.define([], function () {
+sap.ui.define([
+    "sap/m/library"
+], function (mobileLibrary) {
     "use strict";
 
     return {
         formatMail: function (sEID) {
-            return "mailto:" + sEID + "@company.com";
+            if (!sEID) {
+                return "";
+            }
+
+            //var oBundle = sap.ui.getCore().getLibraryResourceBundle("sapips.training.jsonbinding.i18n.i18n");
+            var oBundle = sap.ui.getCore().getModel("i18n").getResourceBundle();
+            var sSubject = oBundle.getText("mailSubject");
+            var sBody = oBundle.getText("mailBody");
+            var sDomain = oBundle.getText("domain");
+
+            return "mailto:" + sEID + sDomain + "?subject=" + encodeURIComponent(sSubject) + "&body=" + encodeURIComponent(sBody);
         },
 
         formatStockValue: function (fUnitPrice, iUnitsInStock, sCurrency) {
             if (!fUnitPrice || !iUnitsInStock) {
                 return "";
             }
+
             var fValue = fUnitPrice * iUnitsInStock;
+
             return new Intl.NumberFormat("en-US", {
                 style: "currency",
                 currency: sCurrency || "USD"
